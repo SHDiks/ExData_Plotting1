09e8370302d47a8d1fb377b533058926fb5388d3
Sander Diks <s.h.diks@me.com>
2015-12-12 23:32:32 +0100

    Added the requested files

---
+++ Plot1.R
@@ -0,0 +1,8 @@
+df <- read.table("./household_power_consumption.txt", sep = ";", header = T)
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+dfs <- subset(df, Date == "2007-02-01")
+dfs <- rbind(dfs, subset(df, Date == "2007-02-02"))
+png(filename = "plot1.png", width = 480, height = 480)
+hist(dfs$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
+dev.off()
